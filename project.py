c76074faa8bd4f3391b150c3f59f93e94bd8a959
SanjuMu <sanjusri.munigala@gmail.com>
2025-01-30 20:31:59 -0500

    project

---
+++ project.py
@@ -0,0 +1,10 @@
+import math
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+print(math.prod(tup1))
+print(math.prod(tup2))
+
+
+# Note to teacher: I wasn't sure if this was one way to write this program. But, if it is wrong please let me know later. 
+
